30660d22437b090c6ae3aa5e5c8c4cf85fd6cd19
Tấn Trung <nguyentantrung1801@gmail.com>
2023-12-11 19:30:59 +0700

    hot fix warranty history

--- ARTHS_Data/Models/Requests/Post/CreateWarrantyModel.cs
+++ ARTHS_Data/Models/Requests/Post/CreateWarrantyModel.cs
@@ -3,7 +3,7 @@
     public class CreateWarrantyModel
     {
         public Guid OrderDetailId { get; set; }
-        public Guid HandledBy { get; set; }
+        public Guid? HandledBy { get; set; }
         public int? ProductQuantity { get; set; }
         public string? RepairDetails { get; set; }
         public int TotalAmount { get; set; }
